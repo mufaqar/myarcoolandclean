e78491deb919400ea96253121cceb0c78887d3fc
Mufaqar <mufaqar@gmail.com>
2026-06-19 19:55:22 +0500

    updated

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  output: 'export',
+  async redirects() {
+    return [
+      { source: '/about-us', destination: '/about', permanent: true },
+      { source: '/about-us/:path*', destination: '/about/:path*', permanent: true },
+      { source: '/contact-us', destination: '/contact', permanent: true },
+      { source: '/contact-us/:path*', destination: '/contact/:path*', permanent: true },
+    ];
+  },
 };
 
 module.exports = nextConfig;
